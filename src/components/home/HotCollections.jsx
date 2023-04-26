42dbc8b3e770083ebc8512431ea56d21d1ead41e
SahilPanhotra <sahilpanhotra@gmail.com>
2023-04-26 23:59:42 +0530

    hit api and display nft

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
-
+import { useEffect } from "react";
+import axios from "axios";
 
 const HotCollections = () => {
-  
+  const [nftData, setNftData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
+      )
+      .then((response) => setNftData(response.data));
+  }, []);
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -16,25 +24,29 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
+          {nftData.slice(0, 4).map((nft, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft_coll">
                 <div className="nft_wrap">
                   <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
+                    <img src={nft.nftImage} className="lazy img-fluid" alt="" />
                   </Link>
                 </div>
                 <div className="nft_coll_pp">
                   <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
+                    <img
+                      className="lazy pp-coll"
+                      src={nft.authorImage}
+                      alt="author"
+                    />
                   </Link>
                   <i className="fa fa-check"></i>
                 </div>
                 <div className="nft_coll_info">
                   <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{nft.title}</h4>
                   </Link>
-                  <span>ERC-192</span>
+                  <span>{nft.code}</span>
                 </div>
               </div>
             </div>
